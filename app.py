bead52c2237a49ea14b9f1d0e17a36596d508fad
TomatoSmash24 <p10sidhu20@gmail.com>
2023-10-16 09:27:53 +0530

    added other pages

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app=Flask(__name__)
 def home():
     return render_template('Home Page.html')
 
+@app.route('/clothes')
+def clothes():
+    return render_template('clothes.html')
+
+@app.route('/toys')
+def toys():
+    return render_template('toys.html')
+
+@app.route('/money')
+def money():
+    return render_template('money donations.html')
+
+@app.route('/household')
+def household():
+    return render_template('household.html')
+
 if __name__ == 'main':
     app.run(debug=True)
