eadcc22a538dbf5e9cfe1672256f1680e684545b
TomatoSmash24 <p10sidhu20@gmail.com>
2023-10-17 16:38:39 +0530

    cleaned up

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3 
 
-app=Flask(__name__)
+app = Flask(__name__) 
+ 
+connect = sqlite3.connect('database.db') 
+connect.execute( 
+	'CREATE TABLE IF NOT EXISTS PARTICIPANTS (name TEXT, email TEXT, city TEXT, country TEXT, phone TEXT)') 
+
+@app.route('/index') 
+def index(): 
+	return render_template('index.html')
+
+@app.route('/join', methods=['GET', 'POST']) 
+def join(): 
+	if request.method == 'POST': 
+		name = request.form['name'] 
+		email = request.form['email'] 
+		city = request.form['city'] 
+		country = request.form['country'] 
+		phone = request.form['phone'] 
+
+		with sqlite3.connect("database.db") as users: 
+			cursor = users.cursor() 
+			cursor.execute("INSERT INTO PARTICIPANTS (name,email,city,country,phone) VALUES (?,?,?,?,?)", (name, email, city, country, phone)) 
+			users.commit() 
+		return render_template("index.html") 
+	else: 
+		return render_template('join.html') 
+
+
+@app.route('/participants') 
+def participants(): 
+	connect = sqlite3.connect('database.db') 
+	cursor = connect.cursor() 
+	cursor.execute('SELECT * FROM PARTICIPANTS') 
+
+	data = cursor.fetchall() 
+	return render_template("participants.html", data=data) 
 
 @app.route('/')
 def home():
@@ -23,5 +59,5 @@ def money():
 def household():
     return render_template('household.html')
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
